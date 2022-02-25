5c48cf8aac1017b8056628a5d6a8009393c86f31
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2022-02-25 13:28:38 +0100

    added zis jumpscares

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
+import horrror from "./horrror.png";
 
 function App() {
   return (
     <div className='flex'>
         <div class="absolute justify-center h-screen w-screen bg-main dark:bg-main-gray">
-            <div className='flex items-center justify-center bg-transparent/5 dark:bg-transparent/20'>
+            <div className='flex  justify-center bg-transparent/5 dark:bg-transparent/20'>
                 <Navbar />
             </div>
-            
-            <div className='flex justify-center w-3/5 h-screen'>
-              todo
+            <div className='flex justify-center'>
+              <div className='flex w-3/5 justify-center '>
+                  <p> Hello MY name is Hightowert i veyrtf like smoke zis VERY Bnice</p>
+                  <img src={horrror}/>
+              </div>
             </div>
+            
         </div>
 
 
